53cf05b8a4e99ed2e0b49e0544823225b1bc7682
kingshuk3006 <kingshuk.sarkar.aiml25@heritageit.edu.in>
2022-06-05 14:38:03 +0530

    drawer error attempt

--- components/DrawerSection.js
+++ components/DrawerSection.js
@@ -68,7 +68,7 @@ const DrawerSection = () => {
         onClose={() => setIsDrawerOpen (false)}
       >
         <Box textAlign="center" role="presentation">
-          <div className="bg-[#000000] min-h-screen h-full flex flex-col text-lgit md:px-8 px-4 md:w-fit w-[80vw] space-y-8 py-4">
+          <div className="bg-[#000000] min-h-screen sm:h-full h-full flex flex-col text-lgit md:px-8 px-4 md:w-fit w-[80vw] space-y-8 py-4">
             <div className="text-white text-3xl flex flex-row items-center justify-between child:cursor-pointer">
               <img src="Images/logo.svg" className="w-36" />
               <HiOutlineX onClick={() => setIsDrawerOpen (false)} />
@@ -85,12 +85,12 @@ const DrawerSection = () => {
               </div>
               {sideNavItem.map (item => {
                 return (
-                  <Link href={item.link} key={item.key}>
-                    <div className="flex flex-row items-center space-x-2 md:text-xl text-lg font-sweet_sans_pro cursor-pointer">
+                  
+                    <div className="flex flex-row items-center space-x-2 md:text-xl text-lg font-sweet_sans_pro cursor-pointer" onClick={()=> window.location.href=`${item.link}`}>
                       <img src={item.icons} className="w-8" />
                       <h1 className="text-white">{item.text}</h1>
                     </div>
-                  </Link>
+                 
                 );
               })}
             </div>
