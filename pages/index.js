ef0aa28dcf7e8a81bb39fe1009fb129322245172
kingshuk3006 <kingsarkar3006@gmail.com>
2022-08-01 21:21:29 +0530

    modal added

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,19 @@ import {useRouter} from 'next/router';
 import Head from 'next/head';
 import BoostButton from '../components/BoostButton';
 import Link from 'next/link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import {useState} from 'react';
+import {AiOutlineCloseCircle} from 'react-icons/ai';
+import {FaCalendarAlt} from 'react-icons/fa';
 
 export default function Home () {
+  const [open, setOpen] = useState (true);
+  const [open2, setOpen2] = useState (false);
+  const handleOpen2 = () => setOpen2 (true);
+  const handleClose = () => setOpen (false);
+  const handleClose2 = () => setOpen2 (false);
   const route = useRouter ().pathname;
   console.log (route);
   return (
@@ -26,8 +37,8 @@ export default function Home () {
       <Navbar path={route} />
       <div className="md:ml-16 md:px-12 px-4">
         <Hero />
-        <Link  href='/event'>
-        <img src="/scholarship.png" className='cursor-pointer mx-auto'/>
+        <Link href="/event">
+          <img src="/scholarship.png" className="cursor-pointer mx-auto" />
         </Link>
         <Webinars webinarData={webinarData} title="Webinars" />
         <MasterClass />
@@ -35,9 +46,89 @@ export default function Home () {
         <CitizenScience />
         <Collaborators />
         <Footer />
-        <BoostButton/>
+        <BoostButton />
       </div>
-
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className="relative"
+      >
+        <div className="focus:outline-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className=" rounded-lg bg-gradient-to-tl from-[#14003a] to-[#320090] relative">
+            <div className="px-4 py-4 flex flex-col justify-center space-y-6 sm:w-[500px] w-[80vw]">
+              <div className="flex justify-between">
+                <h1 className="bg-[#00C2FF] font-bold rounded-md px-4 py-2 -rotate-3 font-Europa_Gro max-w-fit text-2xl">
+                  First Time in INDIA
+                </h1>
+                <AiOutlineCloseCircle
+                  className="text-4xl text-white cursor-pointer"
+                  onClick={handleClose}
+                />
+              </div>
+              <div className="text-white leading-tight">
+                <h1 className="font-sweet_sans_pro font-bold italic text-2xl">
+                  LEARN ROCKETRY FROM ISRO ROCKET SCIENTIST
+                </h1>
+                <h1 className="sm:text-xl font-light">
+                  Three days MasterClass on Rocketry by
+                </h1>
+                <h1 className="sm:text-xl font-light w-3/4">
+                  Prof.R.R.Elavgovan
+                  <span className="sm:text-base text-sm font-light">
+                    (Former ISRO Scientist & Core team member of GSLV MK-1)
+                  </span>
+                </h1>
+              </div>
+              <div className="flex items-center space-x-8">
+                <FaCalendarAlt className="text-3xl text-white" />
+                {' '}
+                <h1 className="text-xl font-bold text-white ">20-22 AUGUST</h1>
+              </div>
+              <h1 className="text-2xl italic font-sweet_sans_pro font-bold text-white leading-tight">
+                LIMITED <br /> SLOTS!!!
+              </h1>
+              <div className="relative w-fit">
+                <button
+                  className="bg-[#00C2FF] text-black font-sweet_sans_pro font-bold italic md:text-lg text-[11px] px-3 py-2 rounded-full relative z-20"
+                  onClick={handleOpen2}
+                >
+                  REGISTER NOW
+                </button>
+                <div className="bg-[#00C2FF] absolute inset-0 blur-md rounded-full" />
+              </div>
+              <div className="relative max-w-fit">
+                <h1 className="font-sweet_sans_pro italic font-bold md:text-xl text-base text-white ">
+                  Only at 250 INR
+                </h1>
+                <div className="absolute bg-white inset-0 blur-3xl" />
+              </div>
+            </div>
+            <img
+              src="/rocket.png"
+              className="absolute bottom-0 right-0 w-[50%]"
+            />
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        open={open2}
+        onClose={handleClose2}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div className="focus:outline-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black md:p-8 p-4 rounded-xl">
+          <div className="flex flex-col justify-center md:space-y-8 space-y-4 child:px-4 child:py-3 child:rounded-md child:bg-[#320090] md:child:text-2xl child:text-lg child:font-sweet_sans_pro child:text-white child:italic child:text-center child:font-bold child:cursor-pointer">
+            <Link href="https://rzp.io/l/RocketryMasterclass">
+              <h1>INDIA</h1>
+            </Link>
+            <Link href="https://pages.razorpay.com/RocketryInternational">
+              <h1>OUTSIDE INDIA</h1>
+            </Link>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
