92d7a9355e9a3de03a95d0a0ecf99906ad2bdd1d
kingshuk3006 <kingsarkar3006@gmail.com>
2022-09-15 11:00:16 +0530

    fade efect left

--- components/Hero.js
+++ components/Hero.js
@@ -58,7 +58,38 @@ const Hero = () => {
         })}
 
       </Swiper> */}
-      <div className="w-10/12 relative mx-auto">
+      {/* <div className="w-10/12  relative mx-auto sm:hidden">
+        <img src="heroimage2.png" className="mx-auto hidden md:block" />
+        <img src="heroimage2screen.png" className="mx-auto md:hidden" />
+        <button className="bg-gradient-to-r from-[#FF3D00] to-[#FFE600]  md:px-4 px-2 md:py-2 py-1 xl:w-[20rem] lg:w-[15rem] md:w-[10rem] w-[8rem] md:text-lg text-xs text-white font-sweet_sans_pro rounded-md text-center absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-[60%]">
+          Watch Now
+        </button>
+        <img
+          src="/floatingIcon/floatingIcon1.png"
+          className="absolute md:top-8 top-0 md:left-24 left-12 floating md:w-16 w-8"
+        />
+        <img
+          src="Images/Webinars/english/person/person2.png"
+          className="absolute top-1/2 md:left-44 left-0 floating md:w-16 w-8"
+        />
+        <img
+          src="Images/Webinars/english/person/person9.png"
+          className="absolute md:bottom-0 top-1/4 md:left-24 floating md:w-16 w-8"
+        />
+        <img
+          src="Images/Webinars/english/person/person7.png"
+          className="absolute md:top-8 top-0 md:right-24 right-12  floating md:w-16 w-8"
+        />
+        <img
+          src="/floatingIcon/floatingIcon2.png"
+          className="absolute top-1/2 md:right-44 right-0 floating md:w-16 w-8"
+        />
+        <img
+          src="Images/Webinars/english/person/person13.png"
+          className="absolute  bottom-0 top-1/4 md:right-24 left-1/2 floating md:w-16 w-8"
+        />
+      </div>
+      <div className="w-10/12 relative mx-auto hidden sm:block">
         <img src="heroimage2.png" className="mx-auto" />
         <button className="bg-gradient-to-r from-[#FF3D00] to-[#FFE600]  md:px-4 px-2 md:py-2 py-1 xl:w-[20rem] lg:w-[15rem] md:w-[10rem] w-[8rem] md:text-lg text-xs text-white font-sweet_sans_pro rounded-md text-center absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-[70%]">
           Watch Now
@@ -87,37 +118,68 @@ const Hero = () => {
           src="Images/Webinars/english/person/person13.png"
           className="absolute  bottom-0 right-24 floating md:w-16 w-8"
         />
-      </div>
-      {/* <div className="w-10/12 relative mx-auto">
-        <img src="heroimage1.png" className="mx-auto" />
-        <button className="bg-gradient-to-r from-[#2400FF] to-[#00D1FF]  px-4 py-2 w-[20rem] text-lg text-white font-sweet_sans_pro rounded-md text-center absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-[65%]">
-          Watch Now
+      </div> */}
+      <div className="w-10/12  relative mx-auto sm:hidden">
+        <img src="heroimage1.png" className="mx-auto hidden md:block" />
+        <img src="heroimage1screen.png" className="mx-auto md:hidden" />
+        <button className="bg-gradient-to-r from-[#2400FF] to-[#00D1FF]  md:px-4 px-2 md:py-2 py-1 xl:w-[20rem] lg:w-[15rem] w-[10rem] md:text-lg text-xs text-white font-sweet_sans_pro rounded-md text-center absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-[65%]">
+        Get Started for free
         </button>
         <img
           src="/floatingIcon/floatingIcon1.png"
-          className="absolute top-8 left-24 floating w-16"
+          className="absolute md:top-8 top-0 md:left-24 left-12 floating md:w-16 w-8"
         />
         <img
           src="Images/Webinars/english/person/person2.png"
-          className="absolute top-1/2 left-44 floating w-16"
+          className="absolute bottom-1/3 md:left-44 left-0 floating md:w-16 w-8"
         />
         <img
           src="Images/Webinars/english/person/person9.png"
-          className="absolute top-full left-24 floating w-16"
+          className="absolute md:bottom-0 top-1/4 md:left-24 floating md:w-16 w-8"
         />
         <img
           src="Images/Webinars/english/person/person7.png"
-          className="absolute top-8 right-24 floating w-16"
+          className="absolute md:top-8 top-0 md:right-24 right-12  floating md:w-16 w-8"
         />
         <img
           src="/floatingIcon/floatingIcon2.png"
-          className="absolute top-1/2 right-44 floating w-16"
+          className="absolute bottom-1/3 md:right-44 right-0 floating md:w-16 w-8"
         />
         <img
           src="Images/Webinars/english/person/person13.png"
-          className="absolute top-full right-24 floating w-16"
+          className="absolute  bottom-0 top-1/4 md:right-24 left-1/2 floating md:w-16 w-8"
         />
-      </div> */}
+      </div>
+      <div className="w-10/12 relative mx-auto hidden sm:block">
+        <img src="heroimage1.png" className="mx-auto" />
+        <button className="bg-gradient-to-r from-[#2400FF] to-[#00D1FF]  md:px-4 px-2 md:py-2 py-1 xl:w-[20rem] lg:w-[15rem] md:w-[10rem] w-[8rem] md:text-lg text-xs text-white font-sweet_sans_pro rounded-md text-center absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-[65%]">
+          Get Started for free
+        </button>
+        <img
+          src="/floatingIcon/floatingIcon1.png"
+          className="absolute md:top-8 top-0 left-24 floating md:w-16 w-8"
+        />
+        <img
+          src="Images/Webinars/english/person/person2.png"
+          className="absolute top-1/2 md:left-44 left-0 floating md:w-16 w-8"
+        />
+        <img
+          src="Images/Webinars/english/person/person9.png"
+          className="absolute  bottom-0 left-24 floating md:w-16 w-8"
+        />
+        <img
+          src="Images/Webinars/english/person/person7.png"
+          className="absolute md:top-8 top-0 right-24 floating md:w-16 w-8"
+        />
+        <img
+          src="/floatingIcon/floatingIcon2.png"
+          className="absolute top-1/2 md:right-44 right-0 floating md:w-16 w-8"
+        />
+        <img
+          src="Images/Webinars/english/person/person13.png"
+          className="absolute  bottom-0 right-24 floating md:w-16 w-8"
+        />
+      </div>
     </div>
   );
 };
