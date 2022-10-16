2d50ccb38d4e9be5cbe058e31a68ac9993ffbf50
kingshuk3006 <kingsarkar3006@gmail.com>
2022-10-16 18:13:02 +0530

    navbar edited

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,12 +7,10 @@ import DrawerSection from "./DrawerSection";
 import Link from "next/link";
 import { FiSearch } from "react-icons/fi";
 import { useState } from "react";
-import {webinarData} from '../dummydb';
-
-
+import { webinarData } from "../dummydb";
 
 const Navbar = ({ active, path }) => {
-  const [searchInput, setSearchInput] = useState ('');
+  const [searchInput, setSearchInput] = useState("");
 
   function handleSearch() {
     if (searchInput === "") {
@@ -40,34 +38,23 @@ const Navbar = ({ active, path }) => {
               className="w-3/4 md:w-full cursor-pointer"
             />
           </Link>
-
-          <div className="hidden md:block">
-            <Link href="https://discord.com/invite/XxuJMhAMaD" passHref>
-              <button className="flex flex-row items-center border-2 border-[#9E00FF] rounded-full font-gilroy text-[#9E00FF] px-2 py-[2px] font-semibold md:text-lg text-sm">
-                <FaDiscord
-                  style={{
-                    fontSize: "30px",
-                    marginLeft: "4px",
-                    marginRight: "4px",
-                  }}
-                />
-                Community
-              </button>
-            </Link>
+          <div className="hidden md:block lg:w-1/2 min-w-[25rem] relative">
+            <input
+              type="text"
+              placeholder="Search for space talks, events, etc..."
+              className="w-full bg-transparent focus:outline-none border border-zinc-500 pl-4 pr-12 py-2 rounded-full font-gilroy text-white"
+              onChange={(e) => setSearchInput(e.target.value)}
+              onKeyPress={(e) => e.key === "Enter" && handleSearch()}
+            />
+            <FiSearch
+              className="text-zinc-300 absolute top-2 right-4 text-2xl cursor-pointer"
+              onClick={handleSearch}
+            />
           </div>
         </div>
-        <div className="hidden md:block lg:w-1/3 min-w-[17rem] relative">
-          <input
-            type="text"
-            placeholder="Search for space talks, events, etc..."
-            className="w-full bg-transparent focus:outline-none border border-zinc-500 pl-4 pr-12 py-2 rounded-full font-gilroy text-white"
-            onChange={(e) => setSearchInput(e.target.value)}
-            onKeyPress={(e) => e.key === "Enter" && handleSearch()}
-          />
-          <FiSearch className="text-zinc-300 absolute top-2 right-4 text-2xl cursor-pointer" onClick={handleSearch}/>
-        </div>
+
         <div className="hidden md:block">
-          <div className="flex flex-row items-center space-x-8 text-lg">
+          {/* <div className="flex flex-row items-center space-x-8 text-lg">
             <Link href="/connect">
               <button
                 className={
@@ -94,6 +81,29 @@ const Navbar = ({ active, path }) => {
               <Link href="https://www.facebook.com/HexStarUniverse">
                 <a>
                   <BsFacebook />
+                </a>
+              </Link>
+            </div>
+          </div> */}
+          <div className="flex justify-between items-center space-x-4">
+            <div className="hidden md:block">
+              <Link href="https://discord.com/invite/XxuJMhAMaD" passHref>
+                <button className="flex flex-row items-center border-2 border-[#9E00FF] rounded-full font-gilroy text-[#9E00FF] px-2 py-[2px] font-semibold md:text-lg text-sm">
+                  <FaDiscord
+                    style={{
+                      fontSize: "30px",
+                      marginLeft: "4px",
+                      marginRight: "4px",
+                    }}
+                  />
+                  Community
+                </button>
+              </Link>
+            </div>
+            <div>
+              <Link href="#">
+                <a className="text-white border-2 font-gilroy border-white rounded-full px-4 py-2 font-semibold">
+                  Apply as Mentor
                 </a>
               </Link>
             </div>
